445b9c01b87565acddac1f0ff3aee80c4d25b22e
tadejpetric <tadej.petric1@gmail.com>
2019-09-05 00:58:18 +0200

    added constexpr to factorial

--- factorial.cpp
+++ factorial.cpp
@@ -10,6 +10,13 @@ struct fact<0> {
     enum { val = 1};
 };
 
+constexpr int fact_constexpr(const int a) {
+    if(not a)
+        return 1;
+    return a * fact_constexpr(a - 1);
+}
+
 int main() {
     std::cout << fact<5>::val << "\n";
+    std::cout << fact_constexpr(5) << "\n";
 }
